a6c120bf25457b2c29f2a117343dfd4c9f6185ec
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2024-05-13 23:19:51 -0300

    Update page.tsx

--- app/[locale]/combos/[slug]/page.tsx
+++ app/[locale]/combos/[slug]/page.tsx
@@ -1,4 +1,3 @@
-
 import ComboBySlug from '@/components/SinglePageCombo/ComboBySlug'
 import CommentSection from '@/components/SinglePageCombo/CommentSection'
 import { getSlugCombo } from '@/lib/actions/comboActions'
