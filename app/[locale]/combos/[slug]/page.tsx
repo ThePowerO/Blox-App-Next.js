a07f609cdc31e627d9a65a313c0f682c8417db0f
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2024-05-24 23:10:36 -0300

    Update page.

--- app/[locale]/combos/[slug]/page.tsx
+++ app/[locale]/combos/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions'
-import ComboBySlug from '@/components/SinglePageCombo/ComboBySlug'
+import ComboBySlug from '@/components/SinglePageCombo/ComboBySlug' 
 import CommentSection from '@/components/SinglePageCombo/CommentSection'
 import { getSlugCombo } from '@/lib/actions/comboActions' 
 import { Combo } from '@/lib/types'
