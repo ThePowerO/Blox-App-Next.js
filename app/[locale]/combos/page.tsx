c0f0ea92d8b91a61160d6344e9db6fc3ffa10af8
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2024-06-11 17:13:09 -0300

    Update page.tsx

--- app/[locale]/combos/page.tsx
+++ app/[locale]/combos/page.tsx
@@ -1,4 +1,4 @@
- //import { sendMail } from '@/lib/mail'
+//import { sendMail } from '@/lib/mail'
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
 import { getServerSession } from 'next-auth';
 import React from 'react'
