6355e2cf64e0c3c2be8fd5df0f17b681043a88d0
ThePowerO <helloynched@gmail.com>
2024-06-22 17:49:21 -0300

    LandingPage_AlmostDone

--- lib/actions/authActions.ts
+++ lib/actions/authActions.ts
@@ -7,7 +7,7 @@ import { compileActivationTemplate, compileResetPasswordTemplate, sendMail } fro
 import { signJwt, verifyJwt } from '../jwt';
 import NoAvatar from '@/public/Icons/noavatar.png';
 
-export async function registerUser( user: Omit<User, "id" | "image" | "emailVerified" | "createdAt" | "updatedAt"> ) {
+export async function registerUser( user: Omit<User, "id" | "image" | "emailVerified" | "createdAt" | "updatedAt" | "stripeCustomerId" | "highlights"> ) {
   const result = await prisma.user.create({
     data: {
       image: NoAvatar.src,
