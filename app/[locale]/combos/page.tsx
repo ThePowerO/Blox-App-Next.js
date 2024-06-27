b65e45081d2f772e14f0f3d396999f988851e396
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2024-06-26 22:43:01 -0300

    Update page.tsx

--- app/[locale]/combos/page.tsx
+++ app/[locale]/combos/page.tsx
@@ -1,4 +1,5 @@
-//import { sendMail } from '@/lib/mail'
+ //import { sendMail } from '@/lib/mail'
+
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
 import CommunityCombos from '@/components/CommunityCombos/CommunityCombos';
 import { getServerSession } from 'next-auth';
