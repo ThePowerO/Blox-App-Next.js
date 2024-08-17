eeaaf0107f0c007282273f2f100dde5dcb23c6b7
ThePowerO <helloynched@gmail.com>
2024-08-17 20:36:46 -0300

    generateStaticParams pages and footer pages

--- components/NavBar/ProfileSelector/ProfileSelector.tsx
+++ components/NavBar/ProfileSelector/ProfileSelector.tsx
@@ -74,7 +74,6 @@ const ProfileSelector = ({ locale }: { locale: string }) => {
   
     } catch (error) {
       console.error(error);
-      return null; // Return null or handle the error as needed
     }
   };
 
