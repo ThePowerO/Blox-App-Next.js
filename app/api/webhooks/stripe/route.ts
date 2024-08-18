5f8a665b64718299b8b8c75636635030e82d74b9
ThePowerO <helloynched@gmail.com>
2024-08-18 17:51:27 -0300

    Stripe route new changes added for stripe webhook

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -3,19 +3,18 @@ import Stripe from "stripe";
 import { stripe } from "@/lib/stripe";
 import prisma from "@/lib/prisma";
 import { add } from "date-fns";
+import { NextApiRequest } from "next";
 
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET as string;
 
-export const POST = async (req: NextRequest) => {
-  const payload = await req.text();
-  const response = JSON.parse(payload);
-
-  const sig = req.headers.get("Stripe-Signature") as string;
+export const POST = async (req: NextApiRequest) => {
+  const sig = req.headers['stripe-signature'];
 
   let event;
 
   try {
-    event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+    const body = await buffer(req);
+    event = stripe.webhooks.constructEvent(body, sig as string, endpointSecret);
 
   } catch (err) {
     return NextResponse.json({ status: "failed", err });
@@ -131,4 +130,20 @@ export const POST = async (req: NextRequest) => {
     }
     
   }
+};
+
+const buffer = (req: NextApiRequest) => {
+  return new Promise<Buffer>((resolve, reject) => {
+    const chunks: Buffer[] = [];
+
+    req.on('data', (chunk: Buffer) => {
+      chunks.push(chunk);
+    });
+
+    req.on('end', () => {
+      resolve(Buffer.concat(chunks));
+    });
+
+    req.on('error', reject);
+  });
 };
