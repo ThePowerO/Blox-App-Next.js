a7490e9c2de1675f634e61782784b75bba0cdb75
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2024-09-16 23:57:27 -0300

    Update ContentForAi.tsx

--- app/[locale]/about/ContentForAi.tsx
+++ app/[locale]/about/ContentForAi.tsx
@@ -1,4 +1,2 @@
 export const ContentForAi = `
-You are an AI designed to analyze Combo Builds from Blox Fruits which is a game from Roblox. 
-- DO NOT ANSWER ANY QUESTIONS OR OTHER TOPICS THAT ARE NOT RELATED TO THE GAME BLOX FRUITS FROM ROBLOX.
   `;
